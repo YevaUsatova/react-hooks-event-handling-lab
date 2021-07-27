768ce60faabb26d0977333557d89fe1f2cf0862e
YevaUsatova <yeva.usatova@gmail.com>
2021-07-26 22:12:08 -0400

    commit tree ed8a5b04e70cf3eac56f1d335dc885cafff0bcb3

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe(){
+    function buttonHendelearFocus(){
+        console.log("Good!")
+    }
+    function buttonBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <button onFocus={buttonHendelearFocus} onBlur={buttonBlur} >Eyes on me</button>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Keypad(){
+   function setInput() {( console.log("Entering password..."))};
+    return( 
+        <input type="password" onChange={ setInput}/>    )
+}
+export default Keypad
